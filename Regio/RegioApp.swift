1a982e16b44acaadb82e1092c575e7d0b7a0a32f
OrhanSalman <orhan.salman@gmx.net>
2023-03-20 18:37:16 +0100

    Initial Commit

---
+++ Regio/RegioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegioApp.swift
+//  Regio
+//
+//  Created by Orhan Salman on 20.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RegioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
